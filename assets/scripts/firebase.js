77dac8bc6472ce772639effc0fcbdf5f2ba6e455
aditi-123-ai <aditijayy@gmail.com>
2021-06-24 21:26:34 +0530

    portfolio aditi yadav

---
+++ assets/scripts/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDFw9lK8ADdQAEUAUB9V1NfVHzFMS2hRCg",
+    authDomain: "portfolio-ede49.firebaseapp.com",
+    projectId: "portfolio-ede49",
+    storageBucket: "portfolio-ede49.appspot.com",
+    messagingSenderId: "248324933568",
+    appId: "1:248324933568:web:7fa7a8253b39f69091aa25",
+    measurementId: "G-FMBXEF8Q0J"
+  };
